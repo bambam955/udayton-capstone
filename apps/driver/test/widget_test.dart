90b82fd41e3331aaa3e61d63fa45f372f1bf73c1
Bennett Moore <bennett@bemoore.life>
2026-02-14 12:29:44 -0500

    fix: update local package name in unit tests

--- apps/driver/test/widget_test.dart
+++ apps/driver/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:izrush_driver/main.dart';
+import 'package:bizrush_driver/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
